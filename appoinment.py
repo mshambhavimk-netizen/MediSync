0af91d5e1535b0e3ea54c3fca645ace919a20182
mshambhavimk-netizen <mshambhavimk@gmail.com>
2026-05-15 08:42:16 +0000

    Replaced location with city and state

--- appoinment.py
+++ appoinment.py
@@ -6,7 +6,8 @@ class Appointment:
         self.doctor_name = doctor_name  
         self.date = date  
         self.time = time  
-        self.location = location  
+        self.city = city  
+        self.state = state  
 
     def schedule(self):  
         return (f"Appointment scheduled for {self.patient_name} with Dr. {self.doctor_name} "  
